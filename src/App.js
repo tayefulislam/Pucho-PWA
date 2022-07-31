cd8a8ee946ee6599ff8d453cbdc43e1e2e97a00d
Fidal <fidal15perfect@gmail.com>
2022-07-31 10:12:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import StartPage from "./pages/StartPage/StartPage";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
